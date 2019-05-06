9ac8dde1eca4fd99724d38ea29f4b644a401488e
Alen Thomas <alenthomas9@gmail.com>
2019-05-06 19:26:52 +0530

    animation delay

--- src/App.js
+++ src/App.js
@@ -11,7 +11,7 @@ function Parent({feeds}) {
 
 function Feed({text}) {
   return(
-    <div className='feed'>{`${text}`}</div>
+    <div className='feed'>{text}</div>
   )
 }
 
@@ -28,18 +28,69 @@ class App extends Component {
 
   render() {
     return (
+      <div className='main'>
+        <p style={{height: '5px', animationDelay: '0s'}}>one</p>
+        <p style={{ height: '8px', animationDelay: '2s'}}>two</p>
+        <p style={{ height: '7px', animationDelay: '4s'}}>three</p>
+        <p style={{ height: '4px', animationDelay: '6s'}}>four</p>
+        <p style={{ height: '5px', animationDelay: '8s'}}>five</p>
+        <p style={{ height: '9px', animationDelay: '10s'}}>six</p>
+        <p style={{ height: '3px', animationDelay: '12s'}}>seven</p>
+        <p style={{animationDelay: '14s'}}>eight</p>
+        
+      </div>
+    )
+    // return (<Marquee />);
+//    return (<Fiddle />);
+    return (
       <div className='container'>
         <Parent feeds={this.state.feeds.slice(0, 33).map((e, i) =>
                                  <Feed text={e.title} key={`${e.id}`}/>)}>
         </Parent>
-        <Parent feeds={this.state.feeds.slice(33, 66).map((e, i) =>
-                                 <Feed text={e.title} key={`${e.id}`}/>)}>
-        </Parent>
-        <Parent feeds={this.state.feeds.slice(66, 100).map((e, i) =>
-                                 <Feed text={e.title} key={`${e.id}`}/>)}>
-        </Parent>
+        {/* <Parent feeds={this.state.feeds.slice(33, 66).map((e, i) => */}
+        {/*                          <Feed text={e.title} key={`${e.id}`}/>)}> */}
+        {/* </Parent> */}
+        {/* <Parent feeds={this.state.feeds.slice(66, 100).map((e, i) => */}
+        {/*                          <Feed text={e.title} key={`${e.id}`}/>)}> */}
+        {/* </Parent> */}
       </div>
     )
   }
 }
+
+function Fiddle() {
+  return(
+    <div className='scroller'>
+      <div className='wrapper'>
+        <div className='card'>
+          <p>Text1</p>
+        </div>
+        <div className='card'>
+          <p>Text2</p>
+        </div>
+        <div className='card'>
+          <p>Text3</p>
+        </div>
+        <div className='card'>
+          <p>Text4</p>
+        </div>
+
+      </div>
+    </div>
+  )
+}
+
+function Marquee() {
+  return (
+    <p className="marquee">
+      <span>this is a</span>
+      <span>simple marquee</span>
+      <span>using css</span>
+      <span>only tech</span>
+      <span>with a delay</span>
+    </p>    
+  )
+}
+
+
 export default App;
